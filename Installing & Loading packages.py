26e3c178553cb58ecd44d46d8a782923ce048883
Steve <stephen.p.oneill.18@gmail.com>
2023-05-13 16:24:27 +0100

    Creating installing and loading package script

---
+++ Installing & Loading packages.py
@@ -0,0 +1,10 @@
+
+"""
+    Installing and loading the necessary packages for the 'Analysis of Investing
+    on YouTube' project
+"""
+
+# Installing (to be commented out once code is run once - included to help re-run analysis for others)
+    # pip install nltk
+
+# Loading the packages
---
+++ Webscraping.py
@@ -0,0 +1,8 @@
+
+"""
+This script is for the Webscraping section of the 'Analysis of Investing on YouTube' project.
+"""
+
+# Running loading script
+
+""" Part 1: Connecting to YouTube API """
