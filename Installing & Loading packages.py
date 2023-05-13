3c8c4945e2c9e62854093a7322ced7377b65314e
Steve <stephen.p.oneill.18@gmail.com>
2023-05-13 16:44:58 +0100

    Creating installing and loading package script

--- Installing & Loading packages.py
+++ Installing & Loading packages.py
@@ -5,6 +5,14 @@
 """
 
 # Installing (to be commented out once code is run once - included to help re-run analysis for others)
-    # pip install nltk
 
-# Loading the packages
+#Loading the packages
+import spacy
+from spacy.lang.en import English
+import selenium
+import gensim
+import lazypredict
+import pandas as pd
+import numpy as np
+import py_youtube
+import nltk
