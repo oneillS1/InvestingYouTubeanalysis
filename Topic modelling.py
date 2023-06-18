038fab89190cbb2b9f828afda50d4e4b3444ba01
Steve <stephen.p.oneill.18@gmail.com>
2023-06-18 13:24:13 +0100

    Start of topic modelling

--- Topic modelling.py
+++ Topic modelling.py
@@ -12,9 +12,16 @@ import nltk
 #nltk.download('stopwords')
 #nltk.download('omw-1.4')
 #nltk.download('wordnet')
-#wn = nltk.WordNetLemmatizer()
+wn = nltk.WordNetLemmatizer()
 from bertopic import BERTopic
 from umap import UMAP
+from matplotlib import pyplot as plt
+
+desired_width = 320
+pd.set_option('display.width', desired_width)
+np.set_printoptions(linewidth = desired_width)
+pd.set_option('display.max_columns', 10)
+
 
 """ 2. Reading in the files """
 video_data_path = "C:/Users/Steve.HAHAHA/Desktop/Dissertation/Final dataset(s) for analysis/cleaned data.csv"
@@ -29,7 +36,44 @@ print(test_short_video_df.head())
 
 """ 3. Topic modelling """
 # Test on 200 short videos
+stopwords = nltk.corpus.stopwords.words('english')
+remove_from_stopwords = ["should", "shouldn't", "must", "mustn't", "need", "needn't", "would", "wouldn't"]
+for word in remove_from_stopwords:
+    if word in stopwords:
+        stopwords.remove(word)
+print(len(stopwords))
+
+# Remove stopwords
+test_short_video_df['transcript_without_stopwords'] = test_short_video_df['Transcript'].apply(lambda x: ' '.join([w for w in x.split() if w.lower() not in stopwords]))
+# # Lemmatization
+test_short_video_df['transcript_lemmatized'] = test_short_video_df['transcript_without_stopwords'].apply(lambda x: ' '.join([wn.lemmatize(w) for w in x.split() if w not in stopwords]))
+# # Take a look at the data
+print(test_short_video_df.head())
+
+# Model building
 umap_model = UMAP(n_neighbors=15, n_components=5, min_dist=0.0, metric='cosine', random_state=100)
 
 topic_model_test = BERTopic(umap_model=umap_model, language="english", calculate_probabilities=True)
-topics, probabilities = topic_model_test.fit_transform(test_short_video_df['Transcript'])
+topics, probabilities = topic_model_test.fit_transform(test_short_video_df['transcript_lemmatized'])
+
+topic_model_test.save("C:/Users/Steve.HAHAHA/Desktop/Dissertation/test_model1")
+print('done')
+print('\n')
+
+topic_model_test1 = BERTopic.load("C:/Users/Steve.HAHAHA/Desktop/Dissertation/test_model1")
+
+print(topic_model_test1.get_topic_info())
+print('\n')
+print(topic_model_test1.get_topic(0))
+print('\n')
+barchart = topic_model_test1.visualize_barchart(top_n_topics = 5)
+plt.savefig("barchart.png")
+print('\n')
+print(topic_model_test1.visualize_term_rank())
+
+print('\n')
+print(topic_model_test1.visualize_topics())
+print('\n')
+print(topic_model_test1.visualize_hierarchy(top_n_topics=5))
+print('\n')
+print('here')
