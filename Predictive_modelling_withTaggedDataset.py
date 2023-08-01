79d18219843b0bcb9d83d719805aa781c1928bf5
Steve <stephen.p.oneill.18@gmail.com>
2023-08-01 16:04:15 +0100

    working out the datasets

--- Predictive_modelling_withTaggedDataset.py
+++ Predictive_modelling_withTaggedDataset.py
@@ -19,6 +19,8 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+import ast
 
 
 
@@ -35,7 +37,7 @@ from sklearn.linear_model import LogisticRegression
 # df_tagged.to_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/Embeddings/Predictive model/Datasets/df_tagged_pm.csv', index=False)
 
 """ 1 b. Adding the embeddings to use as inputs """
-df_tagged = pd.read_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/Embeddings/Predictive model/Datasets/df_tagged_pm.csv')
+#df_tagged = pd.read_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/Embeddings/Predictive model/Datasets/df_tagged_pm.csv')
 # transcripts_tagged_pm = df_tagged['combined_sentence'].tolist()
 #
 # start_time = time.time()
@@ -47,37 +49,63 @@ sentence_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 #
 # df_tagged.to_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/Embeddings/Predictive model/Datasets/df_tagged_pm_embedding_1.csv', index=False)
 
-print(df_tagged.head())
-print(df_tagged.shape)
-print(df_tagged.dtype)
-print(df_tagged.colnames)
-df_tagged['embeddings'] = df_tagged['combined_sentence'].apply(sentence_model.encode)
-print(df_tagged.head())
-print(df_tagged.shape)
-print(df_tagged.dtype)
-print(df_tagged.colnames)
-df_tagged.to_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/Embeddings/Predictive model/Datasets/df_tagged_pm_embedding_1.csv', index=False)
+# print(df_tagged.head())
+# print(df_tagged.shape)
+# print(df_tagged.dtypes)
+# print(df_tagged.columns)
+# df_tagged['embeddings'] = df_tagged['combined_sentence'].apply(sentence_model.encode)
+# print(df_tagged.head())
+# print(df_tagged.shape)
+# print(df_tagged.dtypes)
+# print(df_tagged.columns)
+# df_tagged.to_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/Embeddings/Predictive model/Datasets/df_tagged_pm_embedding_1.csv', index=False)
 
 """ 1 c. Splitting into train, validate, test datasets """
-df_tagged_pm1 = pd.read_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/Embeddings/Predictive model/Datasets/df_tagged_pm_embedding_1.csv')
+# df_tagged_pm1 = pd.read_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/Embeddings/Predictive model/Datasets/df_tagged_pm_embedding_1.csv')
+#
+# df_tagged_pm1['embeddings'] = df_tagged_pm1['embeddings'].apply(lambda x: [float(val) for val in x[1:-1].split()])
+#
+# X = np.array(df_tagged_pm1['embeddings'].to_list())
+# y = np.array(df_tagged_pm1['Advice'].to_list())
+#
+# # Split the data into training and testing sets (for k-fold cross-validation)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+#
+# """ 1 d. Train models on the training dataset """
+# clf = LazyClassifier(verbose=0, ignore_warnings=True, custom_metric=None)
+# models_summary, _ = clf.fit(X_train, X_test, y_train, y_test)
+#
+# print(models_summary)
+# models_summary.to_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/Embeddings/Predictive model/LC_models_summary_pm_embedding_1.csv', index=False)
+#
 
 
-# offset = int(df_tagged_pm1.shape[0] * 0.9)
-# print(offset)
+""" Level 2 """
+df_tagged = pd.read_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/Embeddings/Predictive model/Datasets/df_tagged_pm_embedding_1.csv')
+df_tagged['embeddings'] = df_tagged['embeddings'].apply(lambda x: [float(val) for val in x[1:-1].split()])
 
-X = df_tagged_pm1['embeddings'].to_list()
-y = df_tagged_pm1['Advice'].to_list()
+""" 2 a. Creating dataset with all information on each video (including embeddings of chunks) """
+# Step 1: Pivot the dataframe to convert 'embedding' and 'advice' into columns
+df_tagged_pivot = df_tagged.pivot_table(index='ID', columns=df_tagged.groupby('ID').cumcount(),
+                          values=['embeddings', 'Advice', 'combined_sentence'], aggfunc='first')
 
-# Split the data into training and testing sets (for k-fold cross-validation)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+# Step 2: Flatten the multi-level columns into a single-level
+df_tagged_pivot.columns = [f"{col}_{idx}" for col, idx in df_tagged_pivot.columns]
 
-""" 1 d. Train models on the training dataset """
-model = LogisticRegression()
-model.fit(X_train, y_train)
+# Step 3: Reset the index to make 'ID' a regular column again
+df_tagged_pivot = df_tagged_pivot.reset_index()
+df_tagged_pivot.to_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/Embeddings/Predictive model/df_tagged_pivot.csv', index=False)
+print(df_tagged_pivot.head())
 
-# Make predictions on the test set
-y_pred = model.predict(X_test)
+video_data_chunks_count = pd.read_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/video_data_chunks_count.csv')
+metadata_embeddings_tagged_df_pm = pd.merge(video_data_chunks_count, df_tagged_pivot, on='ID', how='inner')
+metadata_embeddings_tagged_df_pm.to_csv('C:/Users/Steve.HAHAHA/Desktop/Dissertation/Embeddings/Predictive model/Datasets/metadata_embeddings_tagged_df_pm.csv', index=False)
 
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
+""" 2 b. Embed the title and description to use in a predictive model """
+
+
+""" 2 c. Splitting into train, validate, test datasets """
+
+
+""" 2 d. Train models on the training dataset """
+
