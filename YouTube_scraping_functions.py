4fd89da5f9ae590146c41caf39e8194d6b7cd730
Steve <stephen.p.oneill.18@gmail.com>
2023-09-14 22:12:23 +0100

    annotating

--- YouTube_scraping_functions.py
+++ YouTube_scraping_functions.py
@@ -17,7 +17,9 @@ def search_videos_by_keyword(keyword, max_results=100, video_length = 900):
     video_ids = []
     for video in videos_search.result()['result']:
         duration = video.get('duration')
+        # checking video is under 15 mins by using parse_duration() function defined below
         if duration and parse_duration(duration) < video_length:  # Check if duration is under 15 minutes (900 seconds)
+            # just getting the video id here
             video_ids.append(video['id'])
 
     return video_ids
@@ -26,21 +28,22 @@ def search_videos_by_keyword(keyword, max_results=100, video_length = 900):
 def search_videos_by_keyword_in_channel(channel_ids, keyword, max_results=100, api_key=None):
     video_ids_by_channel = {}
     all_video_ids = []
-
+    # using the API key to use youtube.search()
     youtube = build('youtube', 'v3', developerKey=api_key)
 
-    for channel_id in channel_ids:
+    for channel_id in channel_ids: # for each channel, do a keyword search and return a list containing the video id
         video_ids = []
 
         search_response = youtube.search().list(
             part='id',
-            q=keyword,
+            q=keyword, # keyword search
             channelId=channel_id,
             type='video',
             maxResults=max_results,
-            videoDuration='medium'
+            videoDuration='medium' # for 4 - 15 min videos
         ).execute()
 
+        # create a channel id: video id(s) dictionary and a list of all video ids
         for item in search_response['items']:
             video_id = item['id']['videoId']
             video_ids.append(video_id)
@@ -57,7 +60,7 @@ def search_videos_by_keyword_in_channel(channel_ids, keyword, max_results=100, a
 def search_videos_by_keyword_in_channel_short(channel_ids, keyword, max_results=100, api_key=None):
     video_ids_by_channel = {}
     all_video_ids = []
-
+    # using the API key to use youtube.search()
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     for channel_id in channel_ids:
@@ -69,9 +72,10 @@ def search_videos_by_keyword_in_channel_short(channel_ids, keyword, max_results=
             channelId=channel_id,
             type='video',
             maxResults=max_results,
-            videoDuration='short'
+            videoDuration='short' # <4 min videos
         ).execute()
 
+        # create a channel id: video id(s) dictionary and a list of all video ids
         for item in search_response['items']:
             video_id = item['id']['videoId']
             video_ids.append(video_id)
@@ -88,20 +92,21 @@ def search_videos_by_keyword_in_channel_short(channel_ids, keyword, max_results=
 
 """ 3: Function to find YouTube channel ids given the usernames of the channel """
 def find_channel_ids(channel_usernames, api_key):
+    # using the API key to use youtube.search()
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     channel_ids_username = []
     channel_ids = []
 
     for channel_username in channel_usernames:
-        username = channel_username.lstrip('@')  # Remove the "@" symbol if present
+        username = channel_username.lstrip('@')  # Remove the "@" symbol from a user name if it is present - usually at the start
 
         search_response = youtube.search().list(
             part='snippet',
             q=username,
-            type='channel',
+            type='channel', # looking for channels not videos as in other functions
             maxResults=1,
-            fields='items(id,snippet(title,channelId))'
+            fields='items(id,snippet(title,channelId))' # title and channel id of interest
         ).execute()
 
         if 'items' in search_response and len(search_response['items']) > 0:
@@ -109,7 +114,7 @@ def find_channel_ids(channel_usernames, api_key):
             channel_id = item['id']['channelId']
             channel_name = item['snippet']['title']
             channel_ids_username.append((channel_name, channel_id))
-            channel_ids.append(channel_id)
+            channel_ids.append(channel_id) # again creating a dictionary of name and id, and also a full list of channel ids
         else:
             channel_ids_username.append((username, None))
 
@@ -119,21 +124,21 @@ def find_channel_ids(channel_usernames, api_key):
 def extract_metadata(video_id, api_key):
     url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id={video_id}&key={api_key}"
     response = requests.get(url)
-    data = response.json()
+    data = response.json() # obtaining the .json file of the information on each video id identified. Can then search json file to find the information
     video_info = data["items"][0]
 
     # Video metadata
     metadata = {}
-    fields = ['channelId', 'publishedAt', 'tags', 'id', 'title', 'description', 'likeCount', 'viewCount', 'commentCount', 'Transcript']
+    fields = ['channelId', 'publishedAt', 'tags', 'id', 'title', 'description', 'likeCount', 'viewCount', 'commentCount', 'Transcript'] # fields of interest
 
     for field in fields:
         try:
-            if field == 'Transcript':
+            if field == 'Transcript': # transcripts obtained using different api
                 try:
                     metadata[field] = YouTubeTranscriptApi.get_transcript(video_id)
                 except (TranscriptsDisabled, NoTranscriptFound):
                     metadata[field] = None
-            elif field in ['likeCount', 'viewCount', 'commentCount']:
+            elif field in ['likeCount', 'viewCount', 'commentCount']: # some fields in statistics, some in snippet within the json file and id is its own subheading
                 metadata[field] = video_info["statistics"][field]
             else:
                 metadata[field] = video_info["snippet"][field] if field.lower() != 'id' else video_info[
@@ -146,7 +151,7 @@ def extract_metadata(video_id, api_key):
 """ 5: Function to extract metadata from multiple videos (uses function 4) """
 def extract_multiVideo_metadata(video_ids, api_key):
     metadata = []
-    for video_id in video_ids:
+    for video_id in video_ids: # uses function 4 but iteratively through the video ids
         data = extract_metadata(video_id, api_key)
         metadata.append(data)
 
@@ -156,16 +161,16 @@ def extract_multiVideo_metadata(video_ids, api_key):
 def append_metadata_to_csv(metadata, csv_file):
     var_names = ['channelId', 'publishedAt', 'tags', 'id', 'title', 'description', 'likeCount', 'viewCount', 'commentCount', 'Transcript']
 
-    # Create a DataFrame with the metadata
+    # Creating an empty dataframe with relevant headings
     df = pd.DataFrame(metadata, columns=var_names)
 
-    # Replace any newline characters in the 'Transcript' column
+    # Replacing  newline characters in 'Transcript'
     df['Transcript'] = df['Transcript'].apply(lambda x: ' '.join(segment['text'] for segment in x) if x is not None else None)
 
-    # Replace any remaining newline characters in other fields
+    # In some fields there are new line characters e.g., description - these are taken out too
     df = df.replace('\n', ' ', regex=True)
 
-    # Write the DataFrame to a CSV file
+    # Write to dataset - encoding used as there are some emojis in description and transcript fields
     df.to_csv(csv_file, index=False, encoding='utf-8-sig')
 
     return df
@@ -173,10 +178,10 @@ def append_metadata_to_csv(metadata, csv_file):
 """ 7: Parsing duration of YouTube videos """
 def parse_duration(duration):
     parts = duration.split(':')
-    if len(parts) == 3:  # Format: HH:MM:SS
+    if len(parts) == 3:  # i.e., the format of time is HH:MM:SS
         hours, minutes, seconds = parts
-        total_seconds = int(hours) * 3600 + int(minutes) * 60 + int(seconds)
-    else:  # Format: MM:SS
+        total_seconds = int(hours) * 3600 + int(minutes) * 60 + int(seconds) # convert to seconds
+    else:  # the format then = MM:SS
         minutes, seconds = parts
         total_seconds = int(minutes) * 60 + int(seconds)
-    return total_seconds
+    return total_seconds # find seconds as easiest binary threshold for use in other functions
